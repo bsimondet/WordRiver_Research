b0d839bf0b1614fff2c221a9d8da212756fe70e9
BDenzer <denz0045@morris.umn.edu>
2015-04-17 14:32:31 -0500

    Some changes that may be irrelevant when context packs are changed in the database.

--- client/app/wordManager/wordManager.controller.js
+++ client/app/wordManager/wordManager.controller.js
@@ -128,30 +128,42 @@ angular.module('WordRiverApp')
 
 
     //THIS IS THE FUNCTION WE ARE WORKING ON - 4/14/15
+    //$scope.removeFromCategory = function (tile) {
+    //  //console.log('1 of 3');
+    //  for(var i = 0; i < $scope.matchTiles.length; i++){
+    //    console.log('2 of 3');
+    //    //console.log($scope.matchTiles.length);
+    //    if(tile._id == $scope.matchTiles[i]._id){
+    //      //console.log('3 of 3');
+    //      $scope.matchTiles.splice(i, 1);$scope.parsePack = function (contextPack) {
+    //  var data = [];
+    //  for (var i = 0; i < contextPack.tileTags.length; i++) {
+    //    data.push({packName: contextPack.tileTags[i].tagName, tiles: [], _id: contextPack.tileTags[i]._id, highlighted: ""});
+    //  }
+    //
+    //  for (var j = 0; j < contextPack.tileBucket.length; j++) {
+    //    var ids = $scope.idInArray(data, contextPack.tileBucket[j].tileTags);
+    //    if (ids.result) {
+    //      for (var k = 0; k < ids.index.length; k++) {
+    //        data[ids.index[k]].tiles.push(contextPack.tileBucket[j]);
+    //      }
+    //    }
+    //  }
+    //  //console.log(data[0].tiles[0].wordName);
+    //  return data;
+    //}
+    //    }
+    //  }
+    //};
+
     $scope.removeFromCategory = function (tile) {
-      //console.log('1 of 3');
+      console.log('1 of 3');
       for(var i = 0; i < $scope.matchTiles.length; i++){
         console.log('2 of 3');
         //console.log($scope.matchTiles.length);
         if(tile._id == $scope.matchTiles[i]._id){
-          //console.log('3 of 3');
-          $scope.matchTiles.splice(i, 1);$scope.parsePack = function (contextPack) {
-      var data = [];
-      for (var i = 0; i < contextPack.tileTags.length; i++) {
-        data.push({packName: contextPack.tileTags[i].tagName, tiles: [], _id: contextPack.tileTags[i]._id, highlighted: ""});
-      }
-
-      for (var j = 0; j < contextPack.tileBucket.length; j++) {
-        var ids = $scope.idInArray(data, contextPack.tileBucket[j].tileTags);
-        if (ids.result) {
-          for (var k = 0; k < ids.index.length; k++) {
-            data[ids.index[k]].tiles.push(contextPack.tileBucket[j]);
-          }
-        }
-      }
-      //console.log(data[0].tiles[0].wordName);
-      return data;
-    }
+          console.log('3 of 3');
+          $scope.matchTiles.splice(i, 1);
         }
       }
     };
@@ -223,6 +235,8 @@ angular.module('WordRiverApp')
 //    });
 //  });
 //};
+
+
 
 //exports.updatePack = function(req, res, next) {
 //  var userId = req.user._id;
