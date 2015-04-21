1f62b372f70a0240be2df3511ce1d1c83fa350e7
Ben Simondet <simon998@morris.umn.edu>
2015-04-21 12:22:37 -0500

    All things displayed in the middle of the assign words page is alphabetized.

--- client/app/assignWords/assignWords.controller.js
+++ client/app/assignWords/assignWords.controller.js
@@ -1,7 +1,4 @@
 'use strict';
-//alphabetize middle
-//fix category alphabetize
-//wider middle
 
 angular.module('WordRiverApp')
     .controller('AssignWordsCtrl', function ($rootScope, $scope, $http, socket, Auth) {
