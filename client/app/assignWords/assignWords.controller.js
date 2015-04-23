d043b8d09e0e2358267a139783cdca03eba3d48e
Rorschach22 <mcart046@morris.umn.edu>
2015-04-23 13:41:46 -0500

    Assigning individual words to individual students is working corectly.

--- client/app/assignWords/assignWords.controller.js
+++ client/app/assignWords/assignWords.controller.js
@@ -495,7 +495,6 @@ angular.module('WordRiverApp')
           return;
         }
         for(var i = 0; i < $scope.selectedStudents.length; i++){
-          var unmatchedWords = [];
           for(var j = 0; j <$scope.selectedWords.length; j++){
             var match = false;
             for(var h = 0; h < $scope.selectedStudents[i].tileBucket.length; h++){
@@ -504,15 +503,21 @@ angular.module('WordRiverApp')
               }
             }
             if(match == false){
-              unmatchedWords.push($scope.selectedWords[j]._id)
+              $scope.selectedStudents[i].tileBucket.push($scope.selectedWords[j]._id)
             }
           }
-          //console.log()
-          //console.log($scope.selectedStudents[i].tileBucket)
-          ////patch request
-          //$http.patch('/api/students/' + $scope.selectedStudents[i]._id,
-          //  {tileBucket:unmatchedWords}
-          //).success(function(){});
+          //patch request
+          $http.patch('/api/students/' + $scope.selectedStudents[i]._id,
+            {tileBucket:$scope.selectedStudents[i].tileBucket}
+          ).success(function(){
+              $scope.getAll();
+            });
+          if(type == 'tile'){
+            console.log("final step");
+            $scope.displayTileInfo(word);
+          } else {
+            $scope.displayStudentInfo(student);
+          }
         }
 
       }
