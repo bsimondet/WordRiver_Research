7a9bdf5f77a7b861048d9e30bf1ac301d45f49a9
casal033 <casal033@morris.umn.edu>
2015-07-08 17:03:07 -0500

    Class stuff started on new model

--- client/app/myClasses/myClasses.controller.js
+++ client/app/myClasses/myClasses.controller.js
@@ -4,6 +4,8 @@ angular.module('WordRiverApp')
   .controller('JsonFileCtrl', function ($scope, $location, $http, Auth) {
     $scope.currentUser = Auth.getCurrentUser();
     $scope.classArray = [];
+    $scope.groupsArray = [];
+
 
     $scope.getClasses = function(){
       $http.get("/api/users/me").success(function(user){
